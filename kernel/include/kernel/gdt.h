838625bd79b30876ca609c241c8437225ba41249
vm_ecole <vm@ecole.com>
2026-07-10 16:14:57 +0200

    New: create temporay gdt struct

--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -1,8 +1,21 @@
 #ifndef GDT_H
 #define GDT_H
 
+#include <stdint.h>
+
 typedef struct s_gdt {
-	
-}	t_gdt;
+	u8_t	entry;
+	u8_t	base;
+	u8_t	limit;
+	u8_t	access;
+	u8_t	flags;
+}			t_gdt;
+
+static t_gdt gdt[10];
+
+struct t_lgdt {
+	u8_t	base;
+	u32_t	addr;
+};
 
 #endif // GDT_H

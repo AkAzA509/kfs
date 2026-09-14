a4f524a9b9f6bf7989f80ea9c1612d552cbf6960
vm_ecole <177094219+AkAzA509@users.noreply.github.com>
2026-09-14 17:26:46 +0200

    Update: incbin path for the font file (rework the build chain)

--- kernel/arch/i386/asm/bootloader.s
+++ kernel/arch/i386/asm/bootloader.s
@@ -52,7 +52,7 @@ stack_top:
 section .rodata
 global font_data
 font_data:
-	incbin "../../../ressources/fonts/Lat15-VGA16.psf"	; add the binary into the kernel blob with
+	incbin "kernel/ressources/fonts/Lat15-VGA16.psf"	; add the binary into the kernel blob with
 											; the font_data name, that we can retreive in the c
 
 ; The linker script specifies _start as the entry point to the kernel and the

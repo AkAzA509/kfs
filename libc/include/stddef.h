85d344e949f499840ad0fed14a52e06211f8dc00
gavin <177094219+AkAzA509@users.noreply.github.com>
2026-09-19 16:49:11 +0200

    Fix: use a compiler macro to def the type of ssize_t and size_t to be accurate with the platform

--- libc/include/stddef.h
+++ libc/include/stddef.h
@@ -1,8 +1,8 @@
 #ifndef STDDEF_H
 #define STDDEF_H
 
-typedef unsigned long size_t;
-typedef signed int ssize_t;
+typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ssize_t;
 
 #define NULL ((void *)0)
 

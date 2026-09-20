2ea140cbe41122beb5a690a34c8ff9201a5bcb9a
gavin <177094219+AkAzA509@users.noreply.github.com>
2026-09-20 17:09:17 +0200

    Update: add new limits

--- libc/include/limits.h
+++ libc/include/limits.h
@@ -2,6 +2,10 @@
 #define LIMITS_H
 
 #define INT_MAX __INT_MAX__
+#define INT_MIN (-__INT_MAX__ - 1)
+
+#define LONG_MAX __LONG_MAX__
+#define LONG_MIN (-__LONG_MAX__ - 1L)
 
 // #define ULONG_MAX 2000
 #endif // LIMITS_H
